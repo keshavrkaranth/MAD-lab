a41ad287a555bcb40c744b1768b2926d6cd7c082
keshavrkaranth <keshavarkarantha@gmail.com>
2021-04-28 18:24:45 +0530

    added visiting-card

---
+++ 01/app/src/main/java/com/keshavrkaranth/visiting_card/MainActivity.java
@@ -0,0 +1,15 @@
+package com.keshavrkaranth.visiting_card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
